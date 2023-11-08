a3f9757bc236cf4ee08b64470e7b684f2af7a60c
moonxxpower <su551732@gmail.com>
2023-11-09 06:01:33 +0900

    feat)회원 기능 추 회원 기능 추가 (회원가입, 로그인, 정보 조회)

--- src/main/java/com/example/Bver/service/MemberService.java
+++ src/main/java/com/example/Bver/service/MemberService.java
@@ -1,4 +1,105 @@
 package com.example.Bver.service;
 
+import com.example.Bver.common.exception.BaseException;
+import com.example.Bver.common.response.BaseResponseStatus;
+import com.example.Bver.dto.member.response.MemberInfoRes;
+import com.example.Bver.entity.Member;
+import com.example.Bver.repository.MemberRepository;
+import com.example.Bver.utils.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.token.secret}")
+    private String key;
+
+    // 토큰의 유효 시간: 1시간 (60분)
+    // 생성된 토큰은 발급된 후 1시간 동안 유효하며, 이 시간 이후에는 토큰이 만료된다.
+    private Long expireTimeMs = 1000 * 60 * 60l;
+
+    // 회원가입
+    public String join(String userName, String password, String passwordCheck, String nickname, String phoneNum, String email, String address, String memberType) {
+
+        // userName 중복 체크
+        memberRepository.findByUserName(userName)
+                .ifPresent(member -> {
+                    throw new BaseException(BaseResponseStatus.MEMBER_ALREADY_EXISTS);
+                });
+
+        // 올바른 비밀번호를 입력했는지 확인
+        if (!password.equals(passwordCheck)) {
+            throw new BaseException(BaseResponseStatus.PASSWORD_MISMATCH);
+        }
+
+        Member member = Member.builder()
+                .userName(userName)
+                .password(encoder.encode(password))
+                .passwordCheck(encoder.encode(password))
+                .nickname(nickname)
+                .phoneNum(phoneNum)
+                .email(email)
+                .address(address)
+                .memberType(memberType)
+                .build();
+
+        memberRepository.save(member);
+
+       return "success";
+    }
+
+    // 로그인
+    public String login(String userName, String password) {
+
+        // userName이 없는 경우
+        Member selectedMember = memberRepository.findByUserName(userName)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.USERNAME_NOT_FOUND));
+
+        // password가 틀린 경우
+        if (!encoder.matches(password, selectedMember.getPassword())) {
+            throw new BaseException(BaseResponseStatus.INVALID_PASSWORD);
+        }
+
+        // 오류가 발생하지 않는다면 토큰 발행
+        String token = JwtTokenUtil.createToken(selectedMember.getUserName(), key, expireTimeMs);
+
+        return token;
+    }
+
+    // 회원 정보 조회 (닉네임, 전화번호, 주소)
+    public MemberInfoRes getMemberInfo(Long id) {
+
+        Optional<Member> memberOptional = memberRepository.findById(id);
+
+        // 찾는 사용자가 없는 경우
+        if (memberOptional.isPresent()) {
+            Member member = memberOptional.get();
+            return getMemberInfoResDto(member);
+        } else {
+            throw new BaseException(BaseResponseStatus.NON_EXIST_MEMBER);
+        }
+    }
+
+    // Dto 변환
+    public MemberInfoRes getMemberInfoResDto(Member member) {
+
+        MemberInfoRes memberInfoResDto = MemberInfoRes.builder()
+                .id(member.getId())
+                .nickname(member.getNickname())
+                .phoneNum(member.getPhoneNum())
+                .address(member.getAddress())
+                .memberType(member.getMemberType())
+                .build();
+
+        return memberInfoResDto;
+    }
 }
